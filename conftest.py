ec6b986850fd334c5153d3c395a2fec2fd28e835
MikhailLuginin <lugininm@yandex.ru>
2023-11-02 21:33:06 +0300

    add new files

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
+
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: '--language=en' or '--language=ru'")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
+    options = OptionsFirefox()
+    options.set_preference("intl.accept_languages", user_language)
+    #fp = webdriver.FirefoxProfile()
+    #fp.set_preference("intl.accept_languages", user_language)
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(options=options)
+        #browser = webdriver.Firefox(FirefoxProfile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_1(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(button) > 0, '!!!НЕ НАШЕЛ!!!'
+
+
